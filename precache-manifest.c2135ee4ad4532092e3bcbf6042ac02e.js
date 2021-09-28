d9b323048a52decd17e15a4119b16df5492730de
John Moffitt <39016062+whatsyourgithub@users.noreply.github.com>
2021-09-28 10:52:50 -0700

    Updates

---
+++ precache-manifest.c2135ee4ad4532092e3bcbf6042ac02e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef7d95eb9ddd5edcf2415cce5f4dc46",
+    "url": "/vailable-web-app/index.html"
+  },
+  {
+    "revision": "72e7386e05cfe87186d8",
+    "url": "/vailable-web-app/static/js/2.6fd09000.chunk.js"
+  },
+  {
+    "revision": "af5a00f2a0c892a8788e521fff95d77f",
+    "url": "/vailable-web-app/static/js/2.6fd09000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6286748e533bb3bf95d",
+    "url": "/vailable-web-app/static/js/main.27c6e4b4.chunk.js"
+  },
+  {
+    "revision": "1162cbab1e019581d632",
+    "url": "/vailable-web-app/static/js/runtime-main.20bfa667.js"
+  }
+]);
